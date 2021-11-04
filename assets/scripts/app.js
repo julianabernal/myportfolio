6c7c17d781637cd0d701128026a5200849eb495f
JulianaBG <70175102+JulianaBG1@users.noreply.github.com>
2021-11-04 00:39:31 -0500

    main

---
+++ assets/scripts/app.js
@@ -0,0 +1,100 @@
+const Portfolio = (function () {
+  function makeWords() {
+    var words = [
+      {
+        text: "math",
+        weight: 12.3,
+      },
+      {
+        text: "geometry",
+        weight: 8,
+      },
+      {
+        text: "physics",
+        weight: 14,
+      },
+      {
+        text: "Texas hold'em",
+        weight: 3,
+      },
+      {
+        text: "soccer",
+        weight: 7,
+      },
+      {
+        text: "english",
+        weight: 10,
+      },
+      {
+        text: "spanish",
+        weight: 9,
+      },
+      {
+        text: "algebra",
+        weight: 15,
+      },
+      {
+        text: "calculus",
+        weight: 7,
+      },
+      {
+        text: "cloud computing",
+        weight: 10,
+      },
+    ];
+    return words;
+  }
+
+  function makeWordCloud(words) {
+    $(".teaching-domains").jQCloud(words, { delay: 120 });
+  }
+
+  function displayWordCloud() {
+    var count = 1;
+    $(window).on("scroll", function () {
+      var y_scroll_pos = window.pageYOffset;
+      var scroll_pos_test = 2700; // set to whatever you want it to be
+      var words = makeWords();
+      if (y_scroll_pos > scroll_pos_test && count <= 1) {
+        makeWordCloud(words);
+        count++;
+      }
+    });
+  }
+
+  function designForm() {
+    $("#my-modal form").addClass("my-form");
+  }
+
+  function typeAnimation() {
+    Typed.new("#writing-text", {
+      strings: [
+        "am a Mathematician.",
+        "love everything about code.",
+        "am learning about Data Science.",
+        "love what I do.",
+      ],
+      // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+      stringsElement: null,
+      // typing speed
+      typeSpeed: 1,
+      contentType: "text",
+      callback: function () {
+        $("#writing-text").css({
+          color: "#fff",
+          "background-color": "black",
+        });
+      },
+      preStringTyped: function () {},
+      onStringTyped: function () {},
+    });
+  }
+
+  return {
+    displayWordCloud: displayWordCloud,
+    typeAnimation: typeAnimation,
+  };
+})();
+
+Portfolio.displayWordCloud();
+Portfolio.typeAnimation();
